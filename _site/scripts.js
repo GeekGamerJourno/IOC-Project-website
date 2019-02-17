2924c2b122a15920352e6077cc6a9e565239d52c
Kallee Buchanan <adella.thrift@gmail.com>
2019-02-17 09:15:26 +0800

    formspree
    activate formspree

--- _site/scripts.js
+++ _site/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
  // Add smooth scrolling to all links
+ var TIMER = 1000;
+
  $("a").on('click', function(event) {
 
    // Make sure this.hash has a value before overriding default behavior
@@ -14,17 +16,22 @@ $(document).ready(function(){
      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
      $('html, body').animate({
        scrollTop: $(hash).offset().top
-     }, 800, function(){
+     }, TIMER, function(){
 
        // Add hash (#) to URL when done scrolling (default click behavior)
        window.location.hash = hash;
      });
    } // End if
  });
-});
-$( document ).ready(function() {
-   $(".burger-button").click(function(){
-     $(".burger-button").toggleClass("active");
-     $(".burger-menu").toggleClass("active");
-   });
+
+ $(".burger-button").click(function(){
+   $(".burger-button").toggleClass("active");
+   $(".burger-menu").toggleClass("active");
+ });
+
+  $("#scroll-arrow").click(function(){
+    var aid = 'scroll-anchor'
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+  })
 });
